90558e5e9cda85d1a97328a12aaac0c149b93fd5
Michael Ferranti <michael@soundful.com>
2025-05-07 16:39:59 -0700

    adding video

--- main.js
+++ main.js
@@ -61,7 +61,8 @@ function skipIntroVideo() {
 // Listen button -> Linktree
 if (listenBtn) {
   listenBtn.addEventListener('click', () => {
-    window.open('https://sparta.ffm.to/supersonics', '_blank');
+    window.open('sparta.ffm.to/spaceaspirations', '_blank');
+
   });
 }
 
